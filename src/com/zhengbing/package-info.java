63575981811c26c39004a7d8ea8d07fcc5980774
zhengbing_vendor <zhengbing_vendor@sensetime.com>
2020-01-10 16:50:33 +0800

    add   simple  thread   implement

---
+++ src/com/zhengbing/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author zhengbing_vendor
+ * @date 2020/1/10
+ **/
+package com.zhengbing;
---
+++ src/com/zhengbing/thread/base/SimpleThread.java
@@ -0,0 +1,34 @@
+package com.zhengbing.thread.base;
+
+/**
+ * 多线程简单实现
+ * @author zhengbing_vendor
+ * @date 2020/1/10
+ **/
+public class SimpleThread {
+
+    private  static  int   count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Runnable  task = new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i <1000000 ; i++) {
+                    count+=1;
+                }
+            }
+        };
+
+        Thread   t1 = new Thread(task);
+        Thread   t2 = new Thread(task);
+
+        t1.start();
+        t2.start();
+
+        // 等待两个线程执行完成
+        t1.join();
+        t2.join();
+        System.out.println("count:"+count);
+    }
+
+}
